e7a14081034199bda49f4057efd5b9dab102893d
Arturoivancastrob <arturoivancastrob@gmail.com>
2025-01-10 21:39:41 -0700

    Revert "Delete Datos directory"
    This reverts commit 075382126d4b322bd3878fe0d9ee7f0a84b8da8d.

---
+++ Datos/DireccionEnvio.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos
+{
+    [Table("DireccioneEnvio")]
+    public class DirecciónEnvio
+    {
+        [Key]
+        public int DireccionID { get; set; }
+
+        [Required, ForeignKey("FK_DireccionEnvio_Usuarios_Id")]
+        public int UsuarioID { get; set; }
+
+        [Required, MaxLength(300)]
+        public string Direccion { get; set; }
+
+        [MaxLength(50)]
+        public string Ciudad { get; set; }
+
+
+        [Required, MaxLength(50)]
+        public string CodigoPostal { get; set; }
+
+        [NotMapped]
+        public Usuario Usuario { get; set; }
+    }
+}
---
+++ Datos/Pedido.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos
+{
+
+    [Table("Pedidos")]
+    public class Pedido
+
+    {
+        [Key]
+        public int PedidoID { get; set; }
+
+        [Required, ForeignKey("FK_Pedidos_Usuarios_Id")]
+        public int UsuarioID { get; set; }
+
+        [MaxLength(50)]
+        public DateTime FechaPedido { get; set; }
+
+        [Required, MaxLength(50)]
+        public string Estado { get; set; }
+
+        [Required, MaxLength(100)]
+        public string TipoEntrega { get; set; }
+
+        [Required, MaxLength(100)]
+        public decimal Total { get; set; }
+
+
+
+       
+        [NotMapped]
+        public Usuario Usuario { get; set; }
+
+        [NotMapped]
+        public ICollection<DetallePedidos> DetallesPedido { get; set; }
+
+        [NotMapped]
+        public Pago Pago { get; set; }
+    }
+}
